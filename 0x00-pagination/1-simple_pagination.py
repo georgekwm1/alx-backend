9b11bf904306f102f193b711868f6e691fe73903
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-02 00:43:36 +0100

    "Changed '<' to '<=' in pagination condition"

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,7 +35,7 @@ class Server:
         assert isinstance(page_size, int)
         assert page > 0
         assert page_size > 0
-        if (page * page_size) < len(self.dataset()):
+        if (page * page_size) <= len(self.dataset()):
             page_index_range = list(index_range(page, page_size))
             result = self.dataset()[page_index_range[0]:page_index_range[1]]
             return result
