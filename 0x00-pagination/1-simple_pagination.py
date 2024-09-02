e26957257d593f0f8bb637cbf7da5b03b0a904aa
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-02 12:25:07 +0100

    "Remove get_hyper method from Server class"

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -42,20 +42,3 @@ class Server:
             return result
         else:
             return []
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        total_page_len = len(self.dataset())
-        data = self.get_page(page, page_size)
-        next_page = page + 1 if page > 0 else None
-        prev_page = page - 1 if page - 1 > 0 else None
-        total_pages = total_page_len / \
-            page_size if total_page_len % page_size == 0 else math.ceil(
-                total_page_len / page_size) + 1
-        return {
-            'page_size': page_size,
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages
-        }
