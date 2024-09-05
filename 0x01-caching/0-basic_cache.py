92ae07c86dfd29202953071e674a5aa8e18a049d
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-05 15:27:33 +0100

    "Rename 'BaseCaching' to 'base_caching' and remove original file"

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """BasicCache module"""
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """BasicCache module"""
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 from collections import OrderedDict
 
 
