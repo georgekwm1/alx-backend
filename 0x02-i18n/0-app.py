36a29fe12a289c712dadc2f5110c0b04bd1ac649
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-11 00:51:49 +0100

    created a template in the template folder

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    """Render home page"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
