ab8f877ae1a23de1d807b4bdd5036a8659dc33f1
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-11 17:16:30 +0100

    "Formatted get_locale function to split return statement into two lines"

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,7 +19,8 @@ app.config.from_object(Config)
 @babel.localeselector
 def get_locale():
     """Gets locale"""
-    return request.accept_languages.best_match(app.config.from_object(Config.LANGUAGES))
+    return request.accept_languages.best_match(
+        app.config.from_object(Config.LANGUAGES))
 
 
 @app.route("/", methods=["GET"])
