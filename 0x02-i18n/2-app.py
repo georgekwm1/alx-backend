a20f1003b48bf8366d603b282c8e3b402b77f9c7
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-09-11 17:13:22 +0100

    "Added docstring to get_locale function and wrapped 'Hello world' in h1 tags in 0-index.html and 1-index.html templates"

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,6 +18,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """Gets locale"""
     return request.accept_languages.best_match(app.config.from_object(Config.LANGUAGES))
 
 
